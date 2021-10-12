4aced9c0fc6fc409c0ea9f5a8ef9ffddbada4622
נתנאל בשן <nbashan@g.jct.ac.il>
2021-10-12 12:51:42 +0300

    finished stage 2: calculating database standard deviation

--- assignment_2/utilities.py
+++ assignment_2/utilities.py
@@ -1,4 +1,7 @@
-def convert2arffNotCleaned(num_of_files):
+from cmath import sqrt
+
+
+def convert2arffNotCleaned(num_of_files:int) ->None:
     fout=open("hospitalNotCleaned.arff", "w")
     fout.write("@relation patients_temperatures\n")
     fout.write("@attribute ward numeric\n")
@@ -16,24 +19,24 @@ def convert2arffNotCleaned(num_of_files):
     fout.close()
 
 
-def fahrenheitToCelsius(F):
+def fahrenheitToCelsius(F:float)->float:
     return 5/9*(F-32)
 
-
-def isCelsius(C):
+def isCelsius(C:float)->float:
     return 31 < C < 43
 
-def isFahrenheit(F):
+def isFahrenheit(F:float)->float:
     return 88 < F < 110
 
-def highOrLow(degree):
+def highOrLow(degree:float)->str:
     if degree <= 37:
         return "Low"
     else:
         return "High"
 
 
-def convert2arff(num_of_files):
+
+def convert2arff(num_of_files:int) -> None:
     fout=open("hospital.arff", "w")
     fout.write("@relation patients_temperatures\n")
     fout.write("@attribute patients_ID numeric\n")
@@ -56,5 +59,45 @@ def convert2arff(num_of_files):
         fin.close()
     fout.close()
 
-convert2arff(3)
-convert2arffNotCleaned(3)
+
+def stdv(num_file:int) -> float:
+        fin=open("department_" + str(num_file)+".txt", "r")
+        sum = 0
+        num_of_patients = 0
+        for time in range(60*24):
+            s=fin.readline().split()
+            for patient in range(len(s)):
+                degree = float(s[patient])
+                if isFahrenheit(degree):
+                    degree = fahrenheitToCelsius(degree)
+                if isCelsius(degree):
+                    sum +=degree
+                    num_of_patients += 1
+        fin.close()
+
+        fin=open("department_" + str(num_file)+".txt", "r")
+
+        mean = sum / num_of_patients
+
+        variance = 0
+
+        for time in range(60*24):
+            s=fin.readline().split()
+            for patient in range(len(s)):
+                degree = float(s[patient])
+                variance += (degree - mean)**2
+
+        variance = variance/num_of_patients
+
+        dev = sqrt(variance)
+
+        fin.close()
+
+        return  dev
+
+#convert2arff(3)
+#convert2arffNotCleaned(3)
+
+print(stdv(1))
+print(stdv(2))
+print(stdv(3))
